2b58630c8093beb92fccd88007bf5751456674e0
lokesh <lokeshchaudhari7411@gmail.com>
2026-08-27 22:36:09 +0530

    initial commit with first program.

---
+++ Basic Learning/src/HelloWorld.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println("hello world!");
+}
+
+//fun main(args: Array<String>){   // we don't need this args: --- after kotlin 1.3 we can omit.
+//    val input = if(args.size>0) "World" else "Kotlin";
+//    println("Hello $input!"); // $ we use to add any value here like we used variable input;
+//}
